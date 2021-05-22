51ef5dab12fcc5014f6fc7077cc5393c462fa639
Jung haesung <48352078+XxCtrlZxX@users.noreply.github.com>
2021-05-22 20:26:44 +0900

    Delete unnecessary code

---
+++ date.js
@@ -0,0 +1,8 @@
+let date = new Date();
+let y = date.getFullYear();
+let M = date.getMonth() + 1;
+if (M < 10) M = '0' + M;
+let d = date.getDate();
+
+let fullDate = y + '-' + M + '-' + d;
+console.log(fullDate);
