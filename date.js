af32a9f243aa52b84780b3b59d243cc44e8c2f77
Jung haesung <48352078+XxCtrlZxX@users.noreply.github.com>
2021-05-23 17:22:22 +0900

    Fixed getElementById Null

--- date.js
+++ date.js
@@ -1,10 +1,9 @@
-let date = new Date();
-let y = date.getFullYear();
-let M = date.getMonth() + 1;
-if (M < 10) M = '0' + M;
-let d = date.getDate();
+window.onload = () => {
+    let date = new Date();
+    let y = date.getFullYear();
+    let M = date.getMonth() + 1;
+    if (M < 10) M = '0' + M;
+    let d = date.getDate();
 
-let fullDate = y + '-' + M + '-' + d;
-console.log(fullDate);
-
-document.getElementById("#date_").innerHTML = fullDate;
+    document.getElementById("date_").innerHTML = y + '-' + M + '-' + d;
+}
